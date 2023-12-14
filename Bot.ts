8f03f53f333356fe26161f8c7a5cf5a3e6feae39
leo <82462733+jameslee-7@users.noreply.github.com>
2023-12-14 10:30:04 +0800

    优化
    提交注释

--- Bot.ts
+++ Bot.ts
@@ -1,73 +1,79 @@
 import { ethers } from "ethers";
 import { BigNumber } from "@ethersproject/bignumber";
 
+// WETH address
+const WETH = ''; 
 
-const WETH = ''   WETH addess
+// Your wallet Privatekey
+const privatekey = 'Your wallet Privatekey'; 
 
-const privatekey = '  Your wallet Privatekey'https://github.com/jameslee-7/A-Bot-buy-coin/blob/main/BOT.ts
-const Provider = new ethers.providers.JsonRpcProvider('Your RPC')
+// Your RPC Provider
+const Provider = new ethers.providers.JsonRpcProvider('Your RPC');
 
-const mywallet = new ethers.Wallet(privatekey, Provider);
-
-const routeraddess = '';   //uniswap routeraddess
-const routerabi = require(''); uniswap routerabi
+// Uniswap router address
+const routeraddess = ''; 
+// Uniswap router ABI
+const routerabi = require(''); 
 const routercontract = new ethers.Contract(routeraddess, routerabi, mywallet);
 
-const factoryaddess = '';     //uniswap factoryaddes
-const factoryabi = require('');          //uniswap factoryabi
-const factorycontract = new ethers.Contract(factoryaddess,factoryabi,mywallet);
+// Uniswap factory address
+const factoryaddess = ''; 
+// Uniswap factory ABI
+const factoryabi = require(''); 
+const factorycontract = new ethers.Contract(factoryaddess, factoryabi, mywallet);
 
-
+// Listening for 'PairCreated' event
 factorycontract.on('PairCreated', async (token0, token1, pairAddress) => {
-console.log('***********************************************')
-console.log('New Pair Created')
-console.log('token0:',token0)
-console.log('token1:',token1)
-console.log('pairAddress:',pairAddress);
+  console.log('***********************************************')
+  console.log('New Pair Created') // 监听到新的交易对被创建
+  console.log('token0:', token0) // 代币0
+  console.log('token1:', token1) // 代币1
+  console.log('pairAddress:', pairAddress); // 交易对地址
 
-let tokenIn,tokenOut;
+  let tokenIn, tokenOut;
 
-  if(token0 == WETH) {
-  tokenIn = token0; 
-  tokenOut = token1;
-}
-
-if(token1 == WETH) {
-  tokenIn = token1; 
-  tokenOut = token0;
-}
-
-const path = [tokenIn, tokenOut]
-const amountIn =ethers.utils.parseUnits('',18);  //WETH you spend
-const amounts = await routercontract.getAmountsOut(amountIn,path);
-//console.log(ethers.utils.formatUnits(amounts[1],18));
-const amountOutMin = amounts[1].sub(amounts[1].div(10));
-//console.log(ethers.utils.formatUnits(amountOutMin,18));
-console.log('***********************************************')
-console.log('Buying New Token')
-console.log('AmoutIn:',ethers.utils.formatEther(amountIn))
-console.log('AmoutOut:',ethers.utils.formatEther(amountOutMin))
-console.log('***********************************************')
-let gas_price =await Provider.getGasPrice();
-
-let transGasLimit = 230000;  
-
-const tx = await routercontract.swapExactTokensForTokens(
-  amountIn,
-  amountOutMin,
-  path,
-
-  '',  //your  address
-  Date.now() + 1000 * 60 * 10, //10 minutes
-  { 
-    from: '',     //your  address
-    gasLimit:300000,
-    gasPrice: gas_price, 
-    value:ethers.utils.parseEther('0')
+  // Determine input and output tokens based on WETH
+  // 根据 WETH 确定输入和输出的代币
+  if (token0 == WETH) {
+    tokenIn = token0; 
+    tokenOut = token1;
   }
-);
-const receipt = await tx.wait(); 
-console.log('Transaction receipt');
-console.log(receipt);
 
+  if (token1 == WETH) {
+    tokenIn = token1; 
+    tokenOut = token0;
+  }
+
+  const path = [tokenIn, tokenOut]
+  const amountIn = ethers.utils.parseUnits('', 18); // Amount of WETH you spend
+  const amounts = await routercontract.getAmountsOut(amountIn, path);
+  const amountOutMin = amounts[1].sub(amounts[1].div(10));
+
+  console.log('***********************************************')
+  console.log('Buying New Token') // 开始购买新代币
+  console.log('AmoutIn:', ethers.utils.formatEther(amountIn)) // 输入金额
+  console.log('AmoutOut:', ethers.utils.formatEther(amountOutMin)) // 预期输出金额
+  console.log('***********************************************')
+
+  let gas_price = await Provider.getGasPrice();
+  let transGasLimit = 230000;  
+
+  // Swap tokens
+  // 交换代币
+  const tx = await routercontract.swapExactTokensForTokens(
+    amountIn,
+    amountOutMin,
+    path,
+    '', // Your address
+    Date.now() + 1000 * 60 * 10, // 10 minutes
+    { 
+      from: '', // Your address
+      gasLimit: 300000,
+      gasPrice: gas_price, 
+      value: ethers.utils.parseEther('0')
+    }
+  );
+  const receipt = await tx.wait(); 
+  console.log('Transaction receipt');
+  console.log(receipt);
 });
